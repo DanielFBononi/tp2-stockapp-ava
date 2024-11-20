15b25a591f37d1181b355aca312361ef35f06e70
DanielFBononi <dnielbf21@gmail.com>
2024-11-20 09:21:16 -0300

    Paginação no Controller Category

---
+++ StockApp.Application/DTOs/QueryDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace StockApp.Application.DTOs
+{
+    public class QueryDTO
+    {
+        public int? Skip { get; set; } = null;
+        public int? Page {  get; set; } = null;
+
+    }
+}
