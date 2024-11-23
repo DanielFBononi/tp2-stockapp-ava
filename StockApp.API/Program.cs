c759c14b307e99da9b2a08c195d1f80ed5cc22e9
DanielFBononi <dnielbf21@gmail.com>
2024-11-23 13:31:24 -0300

    Atualização na program

--- StockApp.API/Program.cs
+++ StockApp.API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using StockApp.Infra.IoC;
-using Microsoft.Extensions.Caching.StackExchangeRedis;
+using System.Text;
 
 internal class Program
 {
@@ -11,31 +12,69 @@ internal class Program
         // Add services to the container.
         builder.Services.AddInfrastructureAPI(builder.Configuration);
 
-
         builder.Services.AddControllers();
 
+        // Configure Swagger
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
 
-        builder.Services.AddAuthentication("Bearer").AddJwtBearer("Bearer", options =>
-       {
-           options.TokenValidationParameters = new TokenValidationParameters
-           {
-               ValidateAudience = false
-           };
+        builder.Services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = builder.Configuration.GetConnectionString("Redis");
         });
 
+
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+            var securitySchema = new OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            };
+
+            c.AddSecurityDefinition("Bearer", securitySchema);
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    securitySchema,
+                    new[] { "Bearer" }
+                }
+            });
+        });
+
+        // JWT Configuration
+        var jwtSettings = builder.Configuration.GetSection("Jwt");
+        var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+        builder.Services.AddAuthentication("Bearer").AddJwtBearer("Bearer", options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings["Issuer"],
+                ValidAudience = jwtSettings["Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(key)
+            };
+        });
 
         builder.Services.AddAuthorization(options =>
         {
             options.AddPolicy("AdminPolicy", policy =>
                 policy.RequireRole("Admin"));
-        });
-
-
-        builder.Services.AddStackExchangeRedisCache(options =>
-        {
-            options.Configuration = builder.Configuration.GetConnectionString("Redis");
         });
 
         var app = builder.Build();
@@ -52,48 +91,8 @@ internal class Program
         app.UseAuthentication();
         app.UseAuthorization();
 
-
         app.MapControllers();
 
         app.Run();
-
-
-
-
-        // Middleware para Rate Limiting
-
-        var requestCounts = new Dictionary<string, int>();
-        var resetTime = DateTime.UtcNow.AddMinutes(2);
-        var request_limit = 2;
-
-        app.Use(async (context, next) =>
-        {
-            var clienteIp = context.Connection.RemoteIpAddress?.ToString()?? "unknowm";
-
-            if (!requestCounts.ContainsKey(clienteIp))
-            {
-                requestCounts[clienteIp] = 0;
-            }
-            if (DateTime.UtcNow > resetTime)
-            {
-                requestCounts.Clear();
-                resetTime = DateTime.UtcNow.AddMinutes(2);
-            }
-            requestCounts[clienteIp]++;
-
-            if (requestCounts[clienteIp]> request_limit)
-            {
-                context.Response.StatusCode = 429;
-                await context.Response.WriteAsync("Limite de requisições, tente novamente maus tarde");
-                return;
-            }
-            await next();
-        });
-
-        //Middleware de Autorização Baseada em Roles
-
-       
-
-
     }
 }
